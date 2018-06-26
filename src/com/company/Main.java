abc8e11606f9d8cd3c3fac48adc316bb5ca05fa2
sashkkoS4 <sasha.rolko17@gmail.com>
2018-06-26 13:36:39 +0300

    test 22

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Stranger Things!");
+        System.out.println("Stranger Things!");
+        System.out.println("Stranger Things!");
+    }
+}

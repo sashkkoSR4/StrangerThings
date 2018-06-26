9457a257954c6cd88873cb566e8e517c24c3ae76
sashkkoSR4 <sasha.rolko@mail.ru>
2018-06-26 13:48:32 +0300

    test branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,8 @@ public class Main {
         System.out.println("Stranger Things!");
         System.out.println("Stranger Things!");
         System.out.println("Stranger Things!");
+        System.out.println("Stranger Things!");
+        System.out.println("Stranger Things!");
+        System.out.println("Stranger Things!");
     }
 }
